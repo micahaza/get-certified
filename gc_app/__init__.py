0141190de15fc9a85de97d228ed14930823212de
Pető Zoltán <bajgli@gmail.com>
2019-09-08 14:12:30 +0200

    home blueprint

--- gc_app/__init__.py
+++ gc_app/__init__.py
@@ -1,5 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+cors = CORS()
 
 
-def create_app(config):
-    return Flask(__name__)
+def create_app(config_filename):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile(config_filename)
+    initialize_extensions(app)
+    register_blueprints(app)
+    return app
+
+
+def initialize_extensions(app: Flask):
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    cors.init_app(app)
+
+
+def register_blueprints(app: Flask):
+    from gc_app.blueprints import home as home_blueprint
+
+    app.register_blueprint(home_blueprint, url_prefix='/')
---
+++ gc_app/blueprints/__init__.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+
+home = Blueprint('home', __name__)
+
+
+@home.route('/')
+def index():
+    return 'Home', 200
